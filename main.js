b53cf67853ae2c14b1bd9458d349411c593dccb7
Tahara <hydoro-3181@itstudio.co>
2018-10-14 10:48:47 +0900

    Initial commit

---
+++ main.js
@@ -0,0 +1,16 @@
+// グローバルコンポーネント
+// Vue.component('hello-component',{
+// 	template:'<p>Hello</p>'
+// })
+
+//ローカルコンポーネント
+var helloComponent = {
+	template: '<p>Hello</p>'
+}
+
+var app = new Vue({
+	el:'#app',
+	components:{
+	'hello-component': helloComponent
+	}
+})
